397609e6fedeba6e88fd07f209e959ae668256d5
Mahindra <118008518+mahindragajjala@users.noreply.github.com>
2025-06-28 12:45:44 +0530

    Create Shortlived_access_tokens_+_refresh_strategy.go

---
+++ JWT_and_Security/Token_revocation_methods/Shortlived_access_tokens_+_refresh_strategy.go
@@ -0,0 +1,15 @@
+//Shortlived_access_tokens_+_refresh_strategy
+
+/*
+- Short-Lived Access Tokens + Refresh Token Strategy in JWT 
+— one of the most widely used, secure authentication mechanisms today. 
+- This is a stateless and scalable solution used by Google, Facebook, 
+and many modern APIs.
+*/
+/*
+Why this strategy?
+To minimize the impact of:
+                          Token theft
+                          Session hijacking
+                          Long-lived tokens being misused
+*/
