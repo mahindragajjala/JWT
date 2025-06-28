ca86af926f505d24158bc731edab5fc1650a6b36
Mahindra <118008518+mahindragajjala@users.noreply.github.com>
2025-06-28 13:09:37 +0530

    Create CSRF_mitigation.go

---
+++ JWT_and_Security/CSRF_mitigation.go
@@ -0,0 +1,7 @@
+/*
+🔥 Scenario:
+You're building a banking web app. 
+          The user logs in and gets a JWT stored in a cookie. 
+          A hacker’s malicious website tries to trick the user's 
+          browser into transferring money.
+*/
